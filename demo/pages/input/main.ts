e146209ee0c04e5cb82db9f3c85d5e49d1c90469
ghiscoding <gbeaulac@gmail.com>
2024-04-26 21:18:39 -0400

    chore: rollback demo changes for Cypress tests to pass

--- demo/pages/input/main.ts
+++ demo/pages/input/main.ts
@@ -1,5 +1,5 @@
 // import { IOptions } from '@src/types';
-// import { VanillaCalendar } from '@/package/build/vanilla-calendar.min';
+// import VanillaCalendar from '@/package/build/vanilla-calendar.min';
 // import '@/package/build/vanilla-calendar.min.css';
 
 import { IOptions, IVanillaCalendar } from '@src/types';
@@ -16,76 +16,27 @@ const configInput: IOptions = {
 			} else {
 				self.HTMLInputElement.value = '';
 			}
-
-			self.HTMLInputElement.value += ` ${self.selectedTime}`;
-			// self.hide();
+			self.hide();
 		},
 	},
-	date: {
-		// min: '2022-03-20',
-		// today: new Date('2022-04-07T00:00'), // use T00:00 for local date
-	},
-	jumpToSelectedDate: true,
-	toggleSelected: false,
 	settings: {
 		visibility: {
 			positionToInput: 'center',
 		},
-		selection: {
-			time: true, // or 12
-		},
-		range: {
-			min: '2022-01-16',
-			// max: 'today',
-		},
-		selected: {
-			dates: [new Date('2022-01-24')],
-			// 	// dates: ['2023-02-08'],
-			// 	// dates: [new Date('2023-02-08')],
-			// 	// dates: [1626307200000],
-			// 	// month: 3,
-			// 	// year: 2023,
-		},
 	},
 };
-// console.log(configInput);
 
 const configDiv: IOptions = {
 	input: true,
-	type: 'multiple',
-	jumpToSelectedDate: true,
 	actions: {
 		changeToInput(e, self) {
-			if (!self.HTMLInputElement) return;
-			if (self.selectedDates[1]) {
-				self.selectedDates.sort((a, b) => +new Date(a) - +new Date(b));
-				self.HTMLInputElement.textContent = `${self.selectedDates[0]} — ${self.selectedDates[self.selectedDates.length - 1]}`;
-			} else if (self.selectedDates[0]) {
-				self.HTMLInputElement.textContent = self.selectedDates[0];
+			if (!self.selectedDates || !self.HTMLInputElement) return;
+			if (self.selectedDates[0]) {
+				self.HTMLInputElement.innerHTML = self.selectedDates[0];
 			} else {
 				self.HTMLInputElement.textContent = '';
 			}
-		},
-	},
-	settings: {
-		selection: {
-			day: 'multiple-ranged',
-		},
-		visibility: {
-			daysOutside: false,
-			positionToInput: 'auto',
-		},
-		range: {
-			disableGaps: true,
-			edgesOnly: true,
-		},
-		selected: {
-			dates: [new Date('2022-01-07')],
-			// dates: ['2023-02-08'],
-			// dates: [new Date('2023-02-08')],
-			// dates: [1626307200000],
-			// month: 3,
-			// year: 2023,
+			self.hide();
 		},
 	},
 };
@@ -93,12 +44,6 @@ const configDiv: IOptions = {
 document.addEventListener('DOMContentLoaded', () => {
 	const calendarInput = new VanillaCalendar('#calendar-input', configInput);
 	calendarInput.init();
-	setTimeout(() => {
-		calendarInput.changeSetting('range', { min: 'today' });
-		console.log('new options', calendarInput.settings);
-		// calendarInput.changeSetting('selected', { dates: ['2024-03-09'], month: 2 });
-		// console.log('new options', calendarInput.settings);
-	}, 200);
 
 	const calendarDiv = new VanillaCalendar('#calendar-div', configDiv);
 	calendarDiv.init();
--- demo/pages/multiple/main.ts
+++ demo/pages/multiple/main.ts
@@ -1,5 +1,5 @@
 // import { IOptions } from '@src/types';
-// import { VanillaCalendar } from '@/package/build/vanilla-calendar.min';
+// import VanillaCalendar from '@/package/build/vanilla-calendar.min';
 // import '@/package/build/vanilla-calendar.min.css';
 
 import { IOptions } from '@src/types';
@@ -8,19 +8,13 @@ import '@src/styles/vanilla-calendar.css';
 
 const config: IOptions = {
 	type: 'multiple',
-	actions: {
-		clickDay: (e, self) => {
-			console.log(self.selectedDates);
-		},
-	},
 	settings: {
 		selection: {
 			day: 'multiple-ranged',
 		},
 		selected: {
-			dates: ['2023-02-08:2023-02-16'],
-			// month: 3,
-			// year: 2023,
+			month: 3,
+			year: 2023,
 		},
 	},
 };
